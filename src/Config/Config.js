4cb89681f3f30b315fc12a1a087501e6d213c2da
dungnguyen4411 <nichiran87@gmail.com>
2020-06-28 00:12:24 +0700

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -2,7 +2,7 @@ const config = {
     apiKey: "",
     authDomain: "",
     databaseURL: "",
-    projectId: "test-d8a2e",
+    projectId: "",
     storageBucket: "",
     messagingSenderId: "",
     appId: ""
